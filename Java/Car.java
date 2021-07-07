32282ef3034237a6a7150c92e06f87fd3071082c
MarkoCortes <bcortesmarko@gmail.com>
2021-07-07 16:08:31 -0500

    Se agrega encapsulamiento a var Passenger (get y set)

--- Java/Car.java
+++ Java/Car.java
@@ -4,15 +4,25 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    private Integer passenger;
     
     public Car(String license, Account driver){
         this.license=license;
         this.driver=driver;
 
     }
+    public void setPassenger(Integer passenger){
+        if(passenger==4) this.passenger=passenger;
+        else{
+            System.out.println("No. pasajero no es 4");
+        }
+
+    }
+    public Integer getPassenger(){
+        return this.passenger;
+    }
     void printDataCar(){
-        System.out.println("El nombre diriver: "+this.driver.name+" con placa: "+this.license);
+        System.out.println("El nombre diriver: "+this.driver.name+" con placa: "+this.license + " Con asientos: "+this.passenger);
         
     }
     
--- Java/Main.java
+++ Java/Main.java
@@ -3,16 +3,19 @@ package CursoPOOUber.Java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");    
-        Car car = new Car("AMD112",new Account("Maruko", "MR321"));
+        UberX uberX = new UberX("INTEL233", new Account("Marko", "MR343")  , "Nissan", "N343");
+        uberX.setPassenger(4);
+        //uberX.passenger=3; 
+        uberX.printDataCar();
         //car.driver= "Maruko";
         //car.license = "AMD112";
-        car.printDataCar();
+       // car.printDataCar();
 
-        Car car2 = new Car("INTEL231",new Account("Vilu", "VL355"));
+       // Car car2 = new Car("INTEL231",new Account("Vilu", "VL355"));
 
         //car2.driver = "Vilu";
         //car2.license = "INTEL234";
-        car2.printDataCar();        
+       // car2.printDataCar();        
         //System.out.println("La licencia es: "+car.license);
 
     }
